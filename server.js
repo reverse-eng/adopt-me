f06507106eca859a6cdaecd38b9c17a1d24fe4f9
getdigitaldesign <getdigitaldesign@gmail.com>
2020-02-04 22:52:21 -0500

    updated server.js to not use __dirname but use a const baseDir that initializes with __dirname

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const express = require("express");
 const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
+const baseDir = __dirname;
 //app.use(favicon(__dirname + "/dist/favicon.ico"));
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(baseDir));
+app.use(express.static(path.join(baseDir, "dist")));
 app.get("/ping", function(req, res) {
   return res.send("pong");
 });
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(baseDir, "dist", "index.html"));
 });
 app.listen(port);
 console.log(`App Running at ${port}`);
